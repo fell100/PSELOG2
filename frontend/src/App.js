b43e56f665b923c6b7b1a60a2123ea06e0776656
Giulia Borges de Oliveira <64155751+gii-pan@users.noreply.github.com>
2020-04-24 15:00:29 -0300

    msg

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React,{ Component } from "react";
+
+import Header from './components/Header';
+
+import "./styles.css";
+import Main from './pages/main';
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
+export default App;
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import "./styles.css";
+
+const Header = () => <header id="main-header">Ghibli Studio</header>
+
+export default Header;
